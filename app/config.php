025cfa41808b36267978f6afd8bb33a634dc08ce
Milan Drenovac <milandrenovac@gmail.com>
2019-02-03 05:48:43 -0800

    Git ignore test

---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'url' => 'http://polaznik22mvc.inchoo4u.net/',
+    'mode'  => 'development',
+    "db"=>[
+        "host"=>"localhost",
+        "name"=>"social_network",
+        "user"=>"root",
+        "password"=>"admin"
+    ]
+];
