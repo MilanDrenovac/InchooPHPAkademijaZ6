59850dcb28aaea0651b9656df2670036465e082f
Milan Drenovac <milandrenovac@gmail.com>
2019-02-03 12:03:51 -0800

    Removed ,

--- script.sql
+++ script.sql
@@ -6,14 +6,14 @@ create table post(
 id int not null primary key auto_increment,
 content text,
 image_location text,
-ts timestamp DEFAULT CURRENT_TIMESTAMP,
+ts timestamp DEFAULT CURRENT_TIMESTAMP
 )engine=InnoDB;
 
 create table comments(
 id int not null primary key auto_increment,
 postid int,
 content text,
-ts timestamp DEFAULT CURRENT_TIMESTAMP,
+ts timestamp DEFAULT CURRENT_TIMESTAMP
 )engine=InnoDB;
 
 insert into post (content) values ('Evo danas pada kiša opet :('), ('Mokar sam ko pas...');
